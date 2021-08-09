0f45515d5b49eedbd69784b6d5dbb64acdd34304
roblee357 <roblee357@hotmail.com>
2021-08-09 04:01:35 -0400

    changed to port 80

--- application.py
+++ application.py
@@ -92,4 +92,4 @@ if __name__ == "__main__":
     # removed before deploying a production app.
     application.debug = True
 
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port=int("80"))
