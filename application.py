10aa4215b3b87eec85fe2912588b0bbc34e35010
roblee357 <roblee357@hotmail.com>
2021-08-09 04:49:04 -0400

    Added mediapipepsoe folder and switched flask to port 80

--- application.py
+++ application.py
@@ -100,4 +100,4 @@ if __name__ == "__main__":
     # removed before deploying a production app.
     application.debug = True
 
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port=80)
